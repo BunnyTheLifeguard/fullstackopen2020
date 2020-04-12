3931aa2ee4d422446c0e3458b37b27d278f60240
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-04-12 12:15:29 +0200

    4.4 totalLikes test

--- Part 4/Blog list/utils/list_helper.js
+++ Part 4/Blog list/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
 	return 1;
 };
 
+const totalLikes = (blogs) => {
+	const sum =
+		blogs.length === 1
+			? blogs[0].likes
+			: blogs.map((blog) => blog.likes).reduce((a, b) => a + b, 0);
+	return sum;
+};
+
 module.exports = {
 	dummy,
+	totalLikes,
 };
