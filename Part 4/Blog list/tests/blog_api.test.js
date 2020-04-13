2007b78c85993c904b61fe3b3692a76c3060e878
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-04-13 20:07:21 +0200

    4.10 postTest

--- Part 4/Blog list/tests/blog_api.test.js
+++ Part 4/Blog list/tests/blog_api.test.js
@@ -1,8 +1,43 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../index');
+const Blog = require('../models/blog');
 
 const api = supertest(app);
+
+const initialBlogs = [
+	{
+		title: 'Test Blog',
+		author: 'Sir Test',
+		url: 'https://www.lifeofsirtest.com',
+		likes: 42,
+	},
+	{
+		title: 'Compuglobal Hypermeganet',
+		author: 'Homer J. Simpson',
+		url: 'https://www.homersimpson.com',
+		likes: 123,
+	},
+	{
+		title: 'Defend Sparta!',
+		author: 'King Leonidas',
+		url: 'https://www.300.com',
+		likes: 300,
+	},
+];
+
+beforeEach(async () => {
+	await Blog.deleteMany({});
+
+	let blogObject = new Blog(initialBlogs[0]);
+	await blogObject.save();
+
+	blogObject = new Blog(initialBlogs[1]);
+	await blogObject.save();
+
+	blogObject = new Blog(initialBlogs[2]);
+	await blogObject.save();
+});
 
 describe('API tests', () => {
 	test('Blogs are returned as json', async () => {
@@ -22,6 +57,27 @@ describe('API tests', () => {
 		const response = await api.get('/api/blogs');
 
 		expect(response.body[0].id).toBeDefined();
+	});
+
+	test('Verify POST request', async () => {
+		const testBlog = {
+			title: 'Test Test',
+			author: 'Mr Test',
+			url: 'https://www.testoftest.com',
+			likes: 24,
+		};
+
+		await api
+			.post('/api/blogs')
+			.send(testBlog)
+			.expect(201)
+			.expect('Content-Type', /application\/json/);
+
+		const response = await api.get('/api/blogs');
+		const contents = response.body.map((r) => r.title);
+
+		expect(response.body).toHaveLength(initialBlogs.length + 1);
+		expect(contents).toContain('Test Test');
 	});
 });
 
