1f2bf73d922ff68a053b3d462ac907171a1951f4
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-04-25 14:17:11 +0200

    5.1 Login w/ conditional rendering

--- Part 5/bloglist-frontend/src/App.js
+++ Part 5/bloglist-frontend/src/App.js
@@ -1,24 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+	const [blogs, setBlogs] = useState([]);
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+	useEffect(() => {
+		blogService.getAll().then((blogs) => setBlogs(blogs));
+	}, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+	const handleLogin = async (event) => {
+		event.preventDefault();
+		try {
+			const user = await loginService.login({
+				username,
+				password,
+			});
 
-export default App
+			setUser(user);
+			setUsername('');
+			setPassword('');
+		} catch (exception) {
+			console.log('Wrong credentials');
+		}
+	};
+
+	const loginForm = () => (
+		<form onSubmit={handleLogin}>
+			<div>
+				Username
+				<input
+					type="text"
+					value={username}
+					name="Username"
+					onChange={({ target }) => setUsername(target.value)}
+				/>
+			</div>
+			<div>
+				Password
+				<input
+					type="text"
+					value={password}
+					name="Password"
+					onChange={({ target }) => setPassword(target.value)}
+				/>
+			</div>
+			<button type="submit">Login</button>
+		</form>
+	);
+
+	return (
+		<div>
+			{user === null ? (
+				<div>
+					<h2>Log in to application</h2>
+					{loginForm()}
+				</div>
+			) : (
+				<div>
+					<h2>Blogs</h2>
+					<p>{user.name} logged in</p>
+					{blogs.map((blog) => (
+						<Blog key={blog.id} blog={blog} />
+					))}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default App;
