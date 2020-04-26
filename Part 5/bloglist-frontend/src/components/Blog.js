7b9256930552bc6114d32aab649cde0aef405b5d
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-04-26 15:58:43 +0200

    5.7 View blog details

--- Part 5/bloglist-frontend/src/components/Blog.js
+++ Part 5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: 'solid',
+		borderWidth: 1,
+		marginBottom: 5,
+	};
+
+	const [details, setDetails] = useState(false);
+
+	const hideDetails = { display: details ? 'none' : '' };
+	const showDetails = { display: details ? '' : 'none' };
+
+	const toggleDetails = () => setDetails(!details);
+
+	return (
+		<div style={blogStyle}>
+			<div style={hideDetails}>
+				{blog.title} {blog.author} <button onClick={toggleDetails}>View</button>
+			</div>
+			<div style={showDetails}>
+				{blog.title} {blog.author} <button onClick={toggleDetails}>Hide</button>
+				<br />
+				{blog.url}
+				<br />
+				{blog.likes} <button>Like</button>
+				<br />
+				{blog.user === undefined ? '' : blog.user.name}
+			</div>
+		</div>
+	);
+};
+
+export default Blog;
