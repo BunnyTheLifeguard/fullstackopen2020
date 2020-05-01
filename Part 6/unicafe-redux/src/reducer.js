0cdcd890a82ef3ff075061c624dfece22851b809
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-05-01 11:48:17 +0200

    6.1 Impement reducer & tests

--- Part 6/unicafe-redux/src/reducer.js
+++ Part 6/unicafe-redux/src/reducer.js
@@ -1,23 +1,41 @@
 const initialState = {
-  good: 0,
-  ok: 0,
-  bad: 0
-}
+	good: 0,
+	ok: 0,
+	bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
-  switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
-  }
-  
-}
+	console.log(action);
+	switch (action.type) {
+		case 'GOOD':
+			const goodPlusOne = ['good', (initialState.good += 1)];
+			const goodState = Object.fromEntries(
+				Object.entries(state).map(([key, val]) =>
+					key !== 'good' ? [key, val] : goodPlusOne
+				)
+			);
+			return goodState;
+		case 'OK':
+			const okPlusOne = ['ok', (initialState.ok += 1)];
+			const okState = Object.fromEntries(
+				Object.entries(state).map(([key, val]) =>
+					key !== 'ok' ? [key, val] : okPlusOne
+				)
+			);
+			return okState;
+		case 'BAD':
+			const badPlusOne = ['bad', (initialState.bad += 1)];
+			const badState = Object.fromEntries(
+				Object.entries(state).map(([key, val]) =>
+					key !== 'bad' ? [key, val] : badPlusOne
+				)
+			);
+			return badState;
+		case 'ZERO':
+			return state;
+		default:
+			return state;
+	}
+};
 
-export default counterReducer
+export default counterReducer;
