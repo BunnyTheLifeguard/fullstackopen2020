c820b5c037dbe25442d39e624af2690fb55c95eb
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-05-04 10:29:49 +0200

    7.5 Reset input fields

--- Part 7/routed-anecdotes/src/App.js
+++ Part 7/routed-anecdotes/src/App.js
@@ -110,31 +110,31 @@ const CreateNew = (props) => {
 		history.push('/');
 	};
 
+	const handleReset = (e) => {
+		e.preventDefault();
+		content.reset();
+		author.reset();
+		info.reset();
+	};
+
 	return (
 		<div>
 			<h2>create a new anecdote</h2>
-			<form onSubmit={handleSubmit}>
+			<form id="form" onSubmit={handleSubmit}>
 				<div>
 					content
-					<input
-						type={content.type}
-						value={content.value}
-						onChange={content.onChange}
-					/>
+					<input {...content} />
 				</div>
 				<div>
 					author
-					<input
-						type={author.type}
-						value={author.value}
-						onChange={author.onChange}
-					/>
+					<input {...author} />
 				</div>
 				<div>
 					url for more info
-					<input type={info.type} value={info.value} onChange={info.onChange} />
+					<input {...info} />
 				</div>
 				<button>create</button>
+				<button onClick={handleReset}>reset</button>
 			</form>
 		</div>
 	);
