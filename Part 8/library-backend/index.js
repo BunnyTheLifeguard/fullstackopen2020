49beeac979d1cd154054cc3a4a7ecd32608449cb
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-05-08 16:05:35 +0200

    8.3 allAuthors query

--- Part 8/library-backend/index.js
+++ Part 8/library-backend/index.js
@@ -86,10 +86,16 @@ const typeDefs = gql`
 		genres: [String]!
 	}
 
+	type Author {
+		name: String!
+		bookCount: Int!
+	}
+
 	type Query {
 		bookCount: Int!
 		authorCount: Int!
 		allBooks: [Book!]!
+		allAuthors: [Author!]!
 	}
 `;
 
@@ -98,6 +104,12 @@ const resolvers = {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
 		allBooks: () => books,
+		allAuthors: () => authors,
+	},
+
+	Author: {
+		name: (root) => root.name,
+		bookCount: (root) => books.filter((b) => b.author === root.name).length,
 	},
 };
 
