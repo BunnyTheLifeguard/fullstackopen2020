30085e3dbc827470ea9cffe5cc2622011061bbd0
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-05-11 21:24:42 +0200

    8.13 Initial connection to DB

--- Part 8/library-backend/index.js
+++ Part 8/library-backend/index.js
@@ -1,96 +1,115 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, UserInputError, gql } = require('apollo-server');
 const { v4: uuidv4 } = require('uuid');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const Book = require('./models/book');
+const Author = require('./models/author');
 
-let authors = [
-	{
-		name: 'Robert Martin',
-		id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-		born: 1952,
-	},
-	{
-		name: 'Martin Fowler',
-		id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-		born: 1963,
-	},
-	{
-		name: 'Fyodor Dostoevsky',
-		id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-		born: 1821,
-	},
-	{
-		name: 'Joshua Kerievsky', // birthyear not known
-		id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-	},
-	{
-		name: 'Sandi Metz', // birthyear not known
-		id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-	},
-];
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
-let books = [
-	{
-		title: 'Clean Code',
-		published: 2008,
-		author: 'Robert Martin',
-		id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-		genres: ['refactoring'],
-	},
-	{
-		title: 'Agile software development',
-		published: 2002,
-		author: 'Robert Martin',
-		id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-		genres: ['agile', 'patterns', 'design'],
-	},
-	{
-		title: 'Refactoring, edition 2',
-		published: 2018,
-		author: 'Martin Fowler',
-		id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-		genres: ['refactoring'],
-	},
-	{
-		title: 'Refactoring to patterns',
-		published: 2008,
-		author: 'Joshua Kerievsky',
-		id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-		genres: ['refactoring', 'patterns'],
-	},
-	{
-		title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-		published: 2012,
-		author: 'Sandi Metz',
-		id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-		genres: ['refactoring', 'design'],
-	},
-	{
-		title: 'Crime and punishment',
-		published: 1866,
-		author: 'Fyodor Dostoevsky',
-		id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-		genres: ['classic', 'crime'],
-	},
-	{
-		title: 'The Demon ',
-		published: 1872,
-		author: 'Fyodor Dostoevsky',
-		id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-		genres: ['classic', 'revolution'],
-	},
-];
+console.log('Connecting to', config.MONGODB_URI);
+
+mongoose
+	.connect(config.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('Connected to MongoDB'))
+	.catch((error) => console.log('Error connecting to MongoDB:', error.message));
+
+// let authors = [
+// 	{
+// 		name: 'Robert Martin',
+// 		id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
+// 		born: 1952,
+// 	},
+// 	{
+// 		name: 'Martin Fowler',
+// 		id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
+// 		born: 1963,
+// 	},
+// 	{
+// 		name: 'Fyodor Dostoevsky',
+// 		id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
+// 		born: 1821,
+// 	},
+// 	{
+// 		name: 'Joshua Kerievsky', // birthyear not known
+// 		id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
+// 	},
+// 	{
+// 		name: 'Sandi Metz', // birthyear not known
+// 		id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
+// 	},
+// ];
+
+// let books = [
+// 	{
+// 		title: 'Clean Code',
+// 		published: 2008,
+// 		author: 'Robert Martin',
+// 		id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
+// 		genres: ['refactoring'],
+// 	},
+// 	{
+// 		title: 'Agile software development',
+// 		published: 2002,
+// 		author: 'Robert Martin',
+// 		id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
+// 		genres: ['agile', 'patterns', 'design'],
+// 	},
+// 	{
+// 		title: 'Refactoring, edition 2',
+// 		published: 2018,
+// 		author: 'Martin Fowler',
+// 		id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
+// 		genres: ['refactoring'],
+// 	},
+// 	{
+// 		title: 'Refactoring to patterns',
+// 		published: 2008,
+// 		author: 'Joshua Kerievsky',
+// 		id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
+// 		genres: ['refactoring', 'patterns'],
+// 	},
+// 	{
+// 		title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+// 		published: 2012,
+// 		author: 'Sandi Metz',
+// 		id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
+// 		genres: ['refactoring', 'design'],
+// 	},
+// 	{
+// 		title: 'Crime and punishment',
+// 		published: 1866,
+// 		author: 'Fyodor Dostoevsky',
+// 		id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
+// 		genres: ['classic', 'crime'],
+// 	},
+// 	{
+// 		title: 'The Demon ',
+// 		published: 1872,
+// 		author: 'Fyodor Dostoevsky',
+// 		id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
+// 		genres: ['classic', 'revolution'],
+// 	},
+// ];
 
 const typeDefs = gql`
 	type Book {
 		title: String!
-		author: String!
+		author: Author!
 		published: Int!
-		genres: [String]
+		genres: [String!]!
+		id: ID!
 	}
 
 	type Author {
 		name: String!
 		born: Int
-		bookCount: Int!
+		bookCount: Int
+		id: ID!
 	}
 
 	type Query {
@@ -128,16 +147,31 @@ const resolvers = {
 				return books;
 			}
 		},
-		allAuthors: () => authors,
+		allAuthors: (root, args) => Author.find({}),
 	},
 
 	Mutation: {
-		addBook: (root, args) => {
-			const book = { ...args, id: uuidv4() };
-			books = books.concat(book);
-			authors.some((a) => a.name === args.author)
-				? authors
-				: (authors = authors.concat({ name: args.author, id: uuidv4() }));
+		addBook: async (root, args) => {
+			const authors = await Author.find({});
+
+			if (authors.some((a) => a.name !== args.author) || authors.length === 0) {
+				const author = new Author({ name: args.author, id: uuidv4() });
+				await author.save();
+			}
+
+			const name = await Author.findOne({ name: args.author });
+
+			const book = new Book({
+				...args,
+				id: uuidv4(),
+				author: name,
+			});
+
+			try {
+				await book.save();
+			} catch (error) {
+				throw new UserInputError(error.message, { invalidArgs: args });
+			}
 			return book;
 		},
 		editAuthor: (root, args) => {
