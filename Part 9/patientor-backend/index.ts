db600225eda4b5924c74bc14919d7e7cb0397865
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-05-16 12:43:45 +0200

    9.9 Patientor FE/BE ping

--- Part 9/patientor-backend/index.ts
+++ Part 9/patientor-backend/index.ts
@@ -1,14 +1,16 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get("/ping", (_req, res) => {
-  console.log("Ping!");
-  res.send("pong");
+app.get('/api/ping', (_req, res) => {
+	console.log('Ping!');
+	res.send('pong');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
