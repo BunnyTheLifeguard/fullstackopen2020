c1b7ed3e6310eb8d75cb9b08571fc4f9feb289e3
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-05-19 21:47:55 +0200

    9.19 Types for new dataset

--- Part 9/patientor-backend/src/services/patientService.ts
+++ Part 9/patientor-backend/src/services/patientService.ts
@@ -1,42 +1,50 @@
-import patients from '../data/patients';
-import { v4 as uuidv4 } from 'uuid';
+import patients from "../data/patients";
+import { v4 as uuidv4 } from "uuid";
 
-import { withoutSsn, PatientEntry, newPatientEntry } from '../types';
+import { withoutSsn, PatientEntry, newPatientEntry, Patient } from "../types";
 
 const getEntries = (): withoutSsn[] => {
-	return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
-		id,
-		name,
-		dateOfBirth,
-		gender,
-		occupation,
-	}));
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
 };
 
 const findById = (id: string) => {
-	const entry = patients.find((p) => p.id === id);
-	if (!entry) {
-		return undefined;
-	} else if (!('entries' in entry)) {
-		const withEntries = { ...entry, entries: [] };
-		return withEntries;
-	}
-	return entry;
+  const entry = patients.find((p) => p.id === id);
+  if (!entry) {
+    return undefined;
+  } else if (!("entries" in entry)) {
+    const withEntries = { ...entry as PatientEntry, entries: [] };
+    return withEntries;
+  }
+  return entry;
 };
 
 const addEntry = (entry: newPatientEntry): PatientEntry => {
-	const newPatient = {
-		id: uuidv4(),
-		...entry,
-	};
-
-	patients.push(newPatient);
-	return newPatient;
+  if ("entries" in entry) {
+    const newPatient = {
+      id: uuidv4(),
+      ...entry as Patient,
+    };
+    patients.push(newPatient);
+    return newPatient;
+  } else {
+    const newPatient = {
+      id: uuidv4(),
+      entries: [],
+      ...entry as Patient,
+    };
+    patients.push(newPatient);
+    return newPatient;
+  }
 };
 
 export default {
-	getEntries,
-	// getAll,
-	addEntry,
-	findById,
+  getEntries,
+  addEntry,
+  findById,
 };
--- Part 9/patientor-backend/src/types.ts
+++ Part 9/patientor-backend/src/types.ts
@@ -1,39 +1,78 @@
-export interface DiagnoseEntry {
-	code: string;
-	name: string;
-	latin?: string;
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
 }
 
 export interface PatientEntry {
-	id: string;
-	name: string;
-	dateOfBirth: string;
-	ssn: string;
-	gender: string;
-	occupation: string;
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: string;
+  occupation: string;
 }
 
-export type withoutSsn = Omit<PatientEntry, 'ssn'>;
+export type withoutSsn = Omit<PatientEntry, "ssn">;
 
-export type newPatientEntry = Omit<PatientEntry, 'id'>;
+export type newPatientEntry = Omit<PatientEntry, "id">;
 
 export enum Gender {
-	male = 'male',
-	female = 'female',
-	other = 'other',
+  Male = "male",
+  Female = "female",
+  Other = "other",
 }
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
 
 export interface Patient {
-	id: string;
-	name: string;
-	ssn: string;
-	occupation: string;
-	gender: Gender;
-	dateOfBirth: string;
-	entries: Entry[];
+  id: string;
+  name: string;
+  ssn: string;
+  occupation: string;
+  gender: Gender;
+  dateOfBirth: string;
+  entries: Entry[];
 }
 
-export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>;
+export type PublicPatient = Omit<Patient, "ssn" | "entries">;
